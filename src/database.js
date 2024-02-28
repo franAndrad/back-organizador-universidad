62efaa5bd34d01f9656ac30c1981da2ad423716c
franAndrad <franciscoezequielandrade@gmail.com>
2024-02-28 04:28:48 -0300

    se modifico el url

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const url = "mongodb+srv://franandrad:15421-6529fa@cluster1.qtkjl9k.mongodb.net/";
 
-mongoose.connect(url);
+mongoose.connect(url, {useNewUrlPaser: true, useUnifiedTopology: true});
 
 const connection = mongoose.connection;
 
