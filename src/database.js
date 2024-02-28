7e815f9caf9d5c50b249211d687c48637acbf5f3
franAndrad <franciscoezequielandrade@gmail.com>
2024-02-28 04:35:10 -0300

    se modifico el url

--- src/database.js
+++ src/database.js
@@ -1,11 +1,20 @@
 import mongoose from "mongoose";
 
-const url = "mongodb+srv://franandrad:15421-6529fa@cluster1.qtkjl9k.mongodb.net/";
+const url =
+  "mongodb+srv://franandrad:15421-6529fa@cluster1.qtkjl9k.mongodb.net/";
 
-mongoose.connect(url);
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 const connection = mongoose.connection;
 
-connection.once('open',()=>{
-    console.log('BD conectada')
-})
+connection.on("error", (err) => {
+  console.error("Error de conexión a MongoDB:", err);
+});
+
+connection.once("open", () => {
+  console.log("Conexión a la base de datos establecida");
+});
