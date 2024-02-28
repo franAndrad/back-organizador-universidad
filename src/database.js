4f2f7890455ac673f2014769a740b0a03f9180f3
franAndrad <franciscoezequielandrade@gmail.com>
2024-02-28 03:57:25 -0300

    se modifico el url

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017";
+const url = "mongodb+srv://franandrad:15421-6529fa@cluster1.qtkjl9k.mongodb.net/";
 
 mongoose.connect(url);
 
